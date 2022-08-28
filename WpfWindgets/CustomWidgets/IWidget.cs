b4cae994ac230cf2c4ea773fedf52a4cbcb7e493
Anaksimander <a.likhanov@g.nsu.ru>
2022-08-28 12:23:34 +0700

    Add project files.

---
+++ WpfWindgets/CustomWidgets/IWidget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfWindgets.CustomWidgets
+{
+    /// <summary>
+    /// Интерфес виджетов.
+    /// </summary>
+    internal interface IWidget
+    {
+        /// <summary>
+        /// Прозрачность панели.
+        /// </summary>
+        public double BackgroundOpacity { get; set; }
+    }
+}
---
+++ WpfWindgets/CustomWidgets/NoteWidget.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWindgets.CustomWidgets
+{
+    /// <summary>
+    /// Виджет заметок.
+    /// </summary>
+    public partial class NoteWidget : UserControl, IWidget
+    {
+
+
+        /// <summary>
+        /// Инициализарует экземпляр класса <see cref="NoteWidget"/>
+        /// </summary>
+        public NoteWidget()
+        {
+            InitializeComponent();
+        }
+
+        public double BackgroundOpacity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
